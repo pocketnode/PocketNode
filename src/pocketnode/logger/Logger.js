7f50af3c2571633a72aa5f2c06eaf81a199cb005
eDroiid <edroiiid@gmail.com>
2017-11-14 00:43:24 -0800

    COLORSgit add src/pocketnode/logger/Logger.js

--- src/pocketnode/logger/Logger.js
+++ src/pocketnode/logger/Logger.js
@@ -1,6 +1,7 @@
+const TerminalTextFormat = require("../utils/TerminalTextFormat.js");
 require("../utils/StringTools.js");
 
-const DefaultLogLevels = [
+const LogLevels = [
     "emergency",
     "alert",
     "critical",
@@ -15,7 +16,7 @@ class Logger {
     constructor(){
         this.debugging = false;
 
-        DefaultLogLevels.forEach(level => {
+        LogLevels.forEach(level => {
             this[level] = function(message){
                 this.log(level.ucfirst(), message);
             }
@@ -23,8 +24,33 @@ class Logger {
     }
 
     log(level, message){
-        if(level.toLowerCase() === "debug" && this.debugging === false) return; 
-        console.log("["+level+"] "+message);
+        if(level.toLowerCase() === "debug" && this.debugging === false) return;
+        let color = "";
+        switch(level.toLowerCase()){
+            case "emergency":
+            case "alert":
+            case "critical":
+                color = TerminalTextFormat.RED;
+                break;
+            case "error":
+                color = TerminalTextFormat.DARK_RED;
+                break;
+            case "warning":
+                color = TerminalTextFormat.YELLOW;
+                break;
+            case "notice":
+                color = TerminalTextFormat.AQUA;
+                break;
+            case "info":
+                color = TerminalTextFormat.WHITE;
+                break;
+            case "debug":
+            default:
+                color = TerminalTextFormat.GRAY;
+                break;
+        }
+        message = (color === "" ? message : message+TerminalTextFormat.RESET);
+        console.log(color+"["+level+"] "+message);
     }
 
     setDebugging(tf){
