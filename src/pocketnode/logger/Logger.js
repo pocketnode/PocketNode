06f137568855e3698f5d4569c2f24c839ca62807
eDroiid <edroiiid@gmail.com>
2017-11-13 23:55:14 -0800

    make logger nicer

--- src/pocketnode/logger/Logger.js
+++ src/pocketnode/logger/Logger.js
@@ -1,19 +1,29 @@
-const LogLevel = require("./LogLevel.js");
+require("../utils/StringTools.js");
+
+const DefaultLogLevels = [
+    "emergency",
+    "alert";
+    "critical";
+    "error";
+    "warning";
+    "notice";
+    "info";
+    "debug"
+];
 
 class Logger {
     constructor(){
         this.debugging = false;
 
-        var LogLevels = new LogLevel();
-        for(let level in LogLevels){
-            this[LogLevels[level]] = function(message){
-                this.log(level, message);
+        DefaultLogLevels.forEach(level => {
+            this[level] = function(message){
+                this.log(level.ucfirst(), message);
             }
-        }
+        });
     }
 
     log(level, message){
-        if(level === "DEBUG" && this.debugging === false) return; 
+        if(level.toLowerCase() === "debug" && this.debugging === false) return; 
         console.log("["+level+"] "+message);
     }
 
