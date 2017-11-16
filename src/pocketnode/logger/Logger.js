e91d133b91f7dc66469da19f9ad426e41a1c833f
eDroiid <edroiiid@gmail.com>
2017-11-15 19:52:46 -0800

    Make logger nicer. Allow custom colors. TextFormat automattically formatted to TerminalTextFormat.

--- src/pocketnode/logger/Logger.js
+++ src/pocketnode/logger/Logger.js
@@ -1,55 +1,81 @@
+const TextFormat = require("../utils/TextFormat.js");
 const TerminalTextFormat = require("../utils/TerminalTextFormat.js");
-require("../utils/StringTools.js");
-
-const LogLevels = [
-    "emergency",
-    "alert",
-    "critical",
-    "error",
-    "warning",
-    "notice",
-    "info",
-    "debug"
-];
 
 class Logger {
     constructor(){
         this.debugging = false;
-
-        LogLevels.forEach(level => {
-            this[level] = function(message){
-                this.log(level.ucfirst(), message);
-            }
-        });
     }
 
-    log(level, message){
-        if(level.toLowerCase() === "debug" && this.debugging === false) return;
-        let color = "";
-        switch(level.toLowerCase()){
-            case "emergency":
-            case "alert":
-            case "critical":
-                color = TerminalTextFormat.RED;
-                break;
-            case "error":
-                color = TerminalTextFormat.DARK_RED;
-                break;
-            case "warning":
-                color = TerminalTextFormat.YELLOW;
-                break;
-            case "notice":
-                color = TerminalTextFormat.AQUA;
-                break;
-            case "info":
-                color = TerminalTextFormat.WHITE;
-                break;
-            case "debug":
-            default:
-                color = TerminalTextFormat.GRAY;
-                break;
+    emergency(message){
+        return this.log("Emergency", message);
+    }
+
+    alert(message){
+        return this.log("Alert", message);
+    }
+
+    critical(message){
+        return this.log("Critical", message);
+    }
+
+    error(message){
+        return this.log("Error", message);
+    }
+
+    warning(message){
+        return this.log("Warning", message);
+    }
+
+    notice(message){
+        return this.log("Notice", message);
+    }
+
+    info(message){
+        return this.log("Info", message);
+    }
+
+    debug(message){
+        if(!this.debugging) return;
+        return this.log("Debug", message);
+    }
+
+    /**
+     * @param level   String
+     * @param message String
+     * @param color   TerminalTextFormat.COLOR
+     */
+    log(level, message, color){
+        message = TextFormat.toTerminal(message);
+        color = color || "";
+
+        if(color === "") {
+            switch(level.toLowerCase()) {
+                case "emergency":
+                case "alert":
+                case "critical":
+                    color = TerminalTextFormat.RED;
+                    break;
+                case "error":
+                    color = TerminalTextFormat.DARK_RED;
+                    break;
+                case "warning":
+                    color = TerminalTextFormat.YELLOW;
+                    break;
+                case "notice":
+                    color = TerminalTextFormat.AQUA;
+                    break;
+                case "info":
+                    color = TerminalTextFormat.WHITE;
+                    break;
+                case "debug":
+                default:
+                    color = TerminalTextFormat.GRAY;
+                    break;
+            }
         }
+
         message = (color === "" ? message : message+TerminalTextFormat.RESET);
+
         console.log(color+"["+level+"] "+message);
     }
 
