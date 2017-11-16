7fc6f9f126bf6286997ee915b3f4aab281f35c7c
eDroiid <edroiiid@gmail.com>
2017-11-16 00:46:16 -0800

    for later use aswell.

---
+++ src/pocketnode/permission/PermissionLevel.js
@@ -0,0 +1,7 @@
+const PermissionLevel = {};
+PermissionLevel.VISITOR = 0;
+PermissionLevel.MEMBER = 1;
+PermissionLevel.OPERATOR = 2;
+PermissionLevel.CUSTOM = 3;
+
+module.exports = PermissionLevel;
