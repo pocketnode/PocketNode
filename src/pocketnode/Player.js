31bddacaa3e285e2569bd2d2791755064057aa84
eDroid <edroiiid@gmail.com>
2017-11-16 08:00:17 -0800

    Update Player.js

--- src/pocketnode/Player.js
+++ src/pocketnode/Player.js
@@ -1,5 +1,22 @@
-class Player {
+const CommandSender = require("./command/CommandSender.js");
 
+class Player extends CommandSender {
+    initVars(){
+        this.username = "";
+        this.displayName = ""
+    }
+    
+    constructor(){
+        super();
+        this.initVars();
+    }
+    
+    isValidUserName(name){
+		if(name === null){
+			return false;
+		}
+		return name.toLowerCase() !== "rcon" and name.toLowerCase() !== "console" and name.length >= 1;// and name.length <= 16 and preg_match("/[^A-Za-z0-9_ ]/", $name) === 0;
+	}
 }
 
 module.exports = Player;
