8c349be805baf1b23a99ab40a9a2e3490d983454
HittmanA <hittmana@comcast.net>
2017-12-09 11:58:41 -0500

    Add some idiot-proofing.
    - Before loading a plugin dir in the plugins directory, make sure it
    actually *is* a dir
    - Make sure that the plugin dir isn’t empty
    - Make sure that the plugin has a manifest
    - Make sure the manifest is valid (has at least a name)
    - If the manifest doesn’t have a description, permission, or aliases,
    set them to a default value

--- src/pocketnode/plugin/PluginLoader.js
+++ src/pocketnode/plugin/PluginLoader.js
@@ -15,22 +15,49 @@ class PluginLoader {
         let pluginList = [];
         let plugins = [];
         let increment = 0;
-        FileSystem.readdirSync(directory).forEach(file => {
-            pluginList[increment] = file;
-            logger.info("Loaded " + file);
-            increment++;
+        FileSystem.readdirSync(directory).forEach(dirContent => {
+            if(FileSystem.lstatSync(directory + dirContent).isDirectory()){
+                if(FileSystem.readdirSync(directory + dirContent + "/")[0] != null) {
+                    pluginList[increment] = dirContent;
+                    logger.info("Loaded " + dirContent);
+                    increment++;
+                }
+            }
         })
         for(var i = 0; i < pluginList.length; i++) {
             var location = directory + pluginList[i] + "/";
             plugins[i] = require(location + pluginList[i]);
-            var manifest = JSON.parse(FileSystem.readFileSync(location + "manifest.json").toString());
-            plugins[i] = new plugins[i](manifest.name, manifest.description, manifest.permission, manifest.aliases, this.server);
-            this.server.getCommandMap().registerCommand(plugins[i]);
-            if(plugins[i].onEnable != null) {
-              plugins[i].onEnable();
+            if(FileSystem.existsSync(location + "manifest.json")) {
+                var manifest = JSON.parse(FileSystem.readFileSync(location + "manifest.json").toString());
+                manifest = this.checkManifest(manifest);
+                plugins[i] = new plugins[i](manifest.name, manifest.description, manifest.permission, manifest.aliases, this.server);
+                this.server.getCommandMap().registerCommand(plugins[i]);
+                if(plugins[i].onEnable != null) {
+                    plugins[i].onEnable();
+                }
+            } else {
+                throw new Error("No manifest found!");
             }
         }
     }
+
+    checkManifest(manifest){
+        if(manifest.name != null && manifest.name !== "") {
+            if(manifest.description == null) {
+                manifest.description = "No description";
+            }
+            if(manifest.permission == null) {
+                manifest.permission = "";
+            }
+            if(manifest.aliases == null) {
+                manifest.aliases = [];
+            }
+            return manifest;
+        } else {
+            throw new Error("No valid manifest name.");
+        }
+    }
+
 }
 
 module.exports = PluginLoader;
