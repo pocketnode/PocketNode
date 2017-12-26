160af9321ea9c3691452d418ae172db42d6ff226
eDroid <edroiiid@gmail.com>
2017-12-26 13:53:39 -0800

    add CheckTypes to globals

--- src/pocketnode/utils/methods/Globals.js
+++ src/pocketnode/utils/methods/Globals.js
@@ -45,3 +45,21 @@ global.Math.round_php = function(value, precision = 0, mode = "ROUND_HALF_UP"){
     }
     return ((isHalf ? value : Math.round(value)) / m);
 };
+
+/**
+ * CheckTypes
+ * Example: CheckTypes([String, "myString"]); // true
+ *          CheckTypes([String, 12]); // throws TypeError
+ *
+ * @throws {TypeError}
+ * @returns {boolean}
+ */
+global.CheckTypes = function(){
+    if(arguments.length === 0) throw new TypeError("Expecting at least 1 Array. Example: [Object, myObjectVar]");
+    for(let i in arguments){
+        let arg = arguments[i];
+        if(!(arg instanceof Array)) throw new TypeError("Expecting Array, got "+(arg.name ? arg.name : arg.constructor.name));
+        if(!(arg[1] instanceof arg[0]) && ((arg[1].constructor.name === arg[0].name) ? arg[1].constructor !== arg[0] : false)) throw new TypeError("Expecting "+(arg[0].name ? arg[0].name : arg[0].constructor.name)+", got "+(arg[1].name ? arg[1].name : arg[1].constructor.name));
+    }
+    return true;
+};
